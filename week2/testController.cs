1544d60bb40ffd013abda86f4376bbe52088ce03
Tashrif Radin Ali <radin00@hotmail.com>
2024-09-14 15:33:07 -0400

    week2

---
+++ week2/testController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class testController : ControllerBase
+    {
+        [HttpGet(template: "test1")]
+        public string test1()
+        {
+            return "Hello World";
+
+
+        }
+        [HttpGet(template: "test2")]
+        public string test2()
+        {
+            return "Hello 5125";
+        }
+        [HttpPost(template:"test3")]
+        public string test3()
+        {
+
+            return "A post request";
+
+        }
+    }
+}
