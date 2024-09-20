a784b11ff967827666f86bdc030a512254840c4d
Tashrif Radin Ali <radin00@hotmail.com>
2024-09-20 12:52:26 -0400

    week3

--- Assignment1/Assignment1/Controllers/Assignment1.cs
+++ Assignment1/Assignment1/Controllers/Assignment1.cs
@@ -61,6 +61,21 @@ namespace Assignment1.Controllers
 
 
     }
+    //question 5
+    [Route("api/[controller]")]
+
+    public class q5 : ControllerBase
+    {
+        [HttpPost(template: "secret")]
+        public string secret(int secret)
+        {
+            return $"Shh.. the secret is {secret}";
+
+        }
+
+
+    }
+    // question 6
     [Route("api/[controller]")]
     public class q6 : ControllerBase
     {
@@ -69,6 +84,46 @@ namespace Assignment1.Controllers
         {
             side = ((3*Math.Sqrt(3))/2)*Math.Pow(side,2);
             return side ;
+
+        }
+
+
+    }
+    // question 7
+    [Route("api/[controller]")]
+    public class q7 : ControllerBase
+    {
+        [HttpGet(template: "timemachine")]
+        public string days(int days)
+        {
+            DateTime Currentday = DateTime.Now;
+            DateTime Updatedday = Currentday.AddDays(days);
+            string FormatedDate = Updatedday.ToString("yyyy-MM-dd");
+            return FormatedDate;
+
+        }
+
+
+    }
+    // question 8
+    [Route("api/[controller]")]
+    public class q8 : ControllerBase
+    {
+        [HttpPost(template: "squashfellows")]
+        public string Order([FromForm] int Small, [FromForm] int Large)
+        {
+            double TotalSmall = 25.50 * Small;
+            string totalsmallstring = TotalSmall.ToString("F2");
+            double TotalBig = 40.50 * Large;
+            string totalbigstring = TotalBig.ToString("F2");
+            double Subtotal = TotalSmall+TotalBig;
+            string subtotalstring = Subtotal.ToString("F2");
+            double tax = (13.0 / 100.0)*Subtotal;
+            string taxstring = tax.ToString("F2");
+            double total = Subtotal + tax;
+            string totalstring = total.ToString("F2");
+            return $"{Small} Small @ $25.50 = ${totalsmallstring}; {Large} Large @ $40.50 = ${totalbigstring}; Subtotal = ${subtotalstring}; " +
+                $"Tax = ${taxstring} HST; Toal = ${totalstring}";
 
         }
 
--- Assignment1/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ Assignment1/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465f1f6b235ad94252dae30141421c8b7cabdbdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34936fae592e88405ceefa7fc2f64b8f9bcaa528")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
